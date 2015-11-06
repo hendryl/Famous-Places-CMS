e2248e56d6619b47cbec041d7e6aab890666896a
Hendry <hendry@icehousecorp.com>
2015-11-06 09:07:28 +0700

    Clean up unused content

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,13 +6,13 @@ var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
 exports.config = {
   // The address of a running selenium server.
   //seleniumAddress: 'http://localhost:4444/wd/hub',
-  //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
+  //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.jsona
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
   },
-  
+
   baseUrl: 'http://localhost:3000',
 
   // Spec patterns are relative to the current working directly when
