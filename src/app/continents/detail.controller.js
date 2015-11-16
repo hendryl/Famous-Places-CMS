1658d76bc830030aa495c1ffe315a9dacc94cca1
Hendry <hendry@icehousecorp.com>
2015-11-16 18:54:20 +0700

    Create form for detail page

--- src/app/continents/detail.controller.js
+++ src/app/continents/detail.controller.js
@@ -1,6 +1,28 @@
 class ContinentDetailController {
-  constructor($scope, ContinentFactory) {
+  constructor($scope, ContinentFactory, $location, $routeParams) {
     'ngInject';
+
+    $scope.id = 0;
+    $scope.name = "";
+    $scope.isPreparing = true;
+
+    var isNewContinent = _.endsWith($location.path(), 'create');
+    console.log(isNewContinent);
+
+    if(!isNewContinent) {
+      $scope.id = Number($routeParams.id);
+      $scope.header = "Update Continent";
+
+      ContinentFactory.getContinentDetail($scope.id)
+      .then(function(result) {
+        $scope.name = result.data.name;
+        console.log($scope.name);
+        $scope.isPreparing = false;
+      });
+    } else {
+      $scope.header = "New Continent";
+      $scope.isPreparing = false;
+    }
   }
 }
 
