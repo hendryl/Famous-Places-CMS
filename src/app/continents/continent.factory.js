2709d7b05f3509dedcc10118cce2c7c5aac9eea4
Hendry <hendry@icehousecorp.com>
2015-11-16 16:14:30 +0700

    Add create update delete methods to continent factory

--- src/app/continents/continent.factory.js
+++ src/app/continents/continent.factory.js
@@ -9,6 +9,18 @@ class ContinentFactory {
     this.getContinentDetail = function(id) {
       return $http.get(baseURLConfig.rootAPI +'/continents/' + id);
     };
+
+    this.createContinent = function(continent) {
+      return $http.post(baseURLConfig.rootAPI + '/continents/', continent);
+    };
+
+    this.updateContinent = function(id, continent) {
+      return $http.put(baseURLConfig.rootAPI + '/continents/' + id, continent);
+    };
+
+    this.deleteContinent = function(id) {
+      return $http.delete(baseURLConfig.rootAPI + '/continents/' + id);
+    };
   }
 }
 
