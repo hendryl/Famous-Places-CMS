fa538733891142f9cee474e6fe35ba2ac7e87b8c
Hendry <hendry@icehousecorp.com>
2015-11-17 11:53:37 +0700

    Style form and buttons

--- src/app/continents/detail.controller.js
+++ src/app/continents/detail.controller.js
@@ -1,5 +1,5 @@
 class ContinentDetailController {
-  constructor($scope, ContinentFactory, $location, $routeParams) {
+  constructor($scope, ContinentFactory, $location, $routeParams, _) {
     'ngInject';
 
     $scope.id = 0;
@@ -7,22 +7,32 @@ class ContinentDetailController {
     $scope.isPreparing = true;
 
     var isNewContinent = _.endsWith($location.path(), 'create');
-    console.log(isNewContinent);
 
-    if(!isNewContinent) {
+    if (!isNewContinent) {
       $scope.id = Number($routeParams.id);
       $scope.header = "Update Continent";
 
       ContinentFactory.getContinentDetail($scope.id)
-      .then(function(result) {
-        $scope.name = result.data.name;
-        console.log($scope.name);
-        $scope.isPreparing = false;
-      });
+        .then(function(result) {
+          $scope.name = result.data.name;
+          $scope.isPreparing = false;
+        });
     } else {
       $scope.header = "New Continent";
       $scope.isPreparing = false;
     }
+
+    $scope.canSave = function() {
+      return $scope.form.$dirty;
+    }
+
+    $scope.cancel = function() {
+
+    };
+
+    $scope.save = function() {
+
+    };
   }
 }
 
