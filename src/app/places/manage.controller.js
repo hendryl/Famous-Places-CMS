345c64cacc6a363e494135382bfd78806feb8cec
Hendry <hendry@icehousecorp.com>
2015-11-19 17:17:52 +0700

    Fix place deletion view logic

--- src/app/places/manage.controller.js
+++ src/app/places/manage.controller.js
@@ -18,7 +18,7 @@ class ManageController {
       if(result) {
         PlaceFactory.delete(id).then(function(data) {
           $scope.places = _.remove($scope.places, function(place) {
-            return place.country_id !== id;
+            return place.place_id !== id;
           });
 
           toastr.success('Place deleted.');
@@ -29,7 +29,7 @@ class ManageController {
     };
 
     PlaceFactory.getList().success(function(data) {
-      data = _.sortBy(data, "country_id");
+      data = _.sortBy(data, "place_id");
       data = _.each(data, function(place) {
         place.continent = place.continent === null ? "-" : place.continent;
         place.image = place.image === null ? "" : place.image;
