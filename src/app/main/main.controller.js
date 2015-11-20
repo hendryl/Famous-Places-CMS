fec186b550af177ec58f43617d854dcff43d82b2
Hendry <hendry@icehousecorp.com>
2015-11-20 17:20:30 +0700

    Add some content to homepage

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,7 +1,22 @@
 class MainController {
-  constructor () {
+  constructor ($scope, PlaceFactory, CountryFactory, CharacteristicFactory, ContinentFactory) {
     'ngInject';
 
+    PlaceFactory.getList().success(function(data) {
+      $scope.places = data.length;
+    });
+
+    CountryFactory.getList().success(function(data) {
+      $scope.countries = data.length;
+    });
+
+    ContinentFactory.getList().success(function(data) {
+      $scope.continents = data.length;
+    });
+
+    CharacteristicFactory.getList().success(function(data) {
+      $scope.characteristics = data.length;
+    });
   }
 }
 
