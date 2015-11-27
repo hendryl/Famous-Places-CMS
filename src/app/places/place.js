75a97fb3b392d817b158fcd3564296537e3503f1
Hendry <hendry@icehousecorp.com>
2015-11-27 11:29:49 +0700

    Fix place not having photo_id property

--- src/app/places/place.js
+++ src/app/places/place.js
@@ -5,7 +5,7 @@ class Place {
       this.enabled = data.enabled;
       this.description = data.description;
       this.country_id = data.country_id;
-      this.image = data.image;
+      this.photo_id = data.photo_id;
       this.latitude = data.latitude;
       this.longitude = data.longitude;
       this.link = data.link;
@@ -15,7 +15,7 @@ class Place {
       this.enabled = true;
       this.description = "";
       this.country_id = null;
-      this.image = "";
+      this.photo_id = null;
       this.latitude = 0;
       this.longitude = 0;
       this.link = "";
