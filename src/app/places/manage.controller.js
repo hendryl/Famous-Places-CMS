cb33612a6f9b8e26e0065db18b4133ecd9543d4a
Hendry <email.hendry.lee@gmail.com>
2015-12-29 11:11:49 +0700

    Add image checker and has image column

--- src/app/places/manage.controller.js
+++ src/app/places/manage.controller.js
@@ -20,6 +20,10 @@ class ManageController {
         column:'latitude'
       },
       {
+        title:'Has image',
+        column: 'hasImage'
+      },
+      {
         title:'Enabled',
         column:'enabled'
       },
@@ -58,8 +62,10 @@ class ManageController {
     PlaceFactory.getList().success(function(data) {
       data = _.sortBy(data, "place_id");
       data = _.each(data, function(place) {
-        place.continent = place.continent === null ? "-" : place.continent;
-        place.image = place.image === null || place.image === undefined ? "" : place.image;
+
+        place.hasImage = place.photo_id == null
+        ? false
+        : true;
       });
       $scope.places = data;
     });
